8f9554ea28605e215f87a052b609620cce6ae8e1
Kotaro <kisobe0609@gmail.com>
2020-08-01 10:22:43 +0900

    welcomeページ編集

--- src/laravel/resources/views/welcome.blade.php
+++ src/laravel/resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 20px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -81,18 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    > Laravel
+                    化学の質問箱
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">理論化学</a>
+                    <a href="#">有機化学</a>
+                    <a href="#">無機化学</a>
+                    <a href="#">高分子化合物</a>
+                    <a href="#">天然有機物</a>
                 </div>
             </div>
         </div>
