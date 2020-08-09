0d4158d2106925c2b468e0bd4f4d861aa1d9ccd8
Kotaro <kisobe0609@gmail.com>
2020-08-09 10:48:43 +0900

    日付表示形式変更

--- src/laravel/resources/views/posts/index.blade.php
+++ src/laravel/resources/views/posts/index.blade.php
@@ -10,17 +10,19 @@
 <div class="container post">
   <span class="btn btn-danger">{{ $post->category->category_name}}</span>
   <h3>{{ $post->title }}</h3>
-  {{ $post->content }}
+  {{ $post->content }}<br>
+  {{ $post->created_at->format('Y年m月d日 H:i:s') }}
   <hr>
   <form action="{{ route('replies.store') }}" method="POST">
     @csrf
+    <input type="hidden" name="author_id" value="{{ Auth::id() ?? 0 }}"
     <input type="hidden" name="post_id" value="{{ $post->id }}">
-    <textarea name="content">
-    </textarea>
-    <input type="submit" value="回答する" class="btn btn-success">
+    <textarea name="content"></textarea>
+    <input type="submit" value="回答する" class="btn btn-primary">
     <hr>
     @foreach($post->replies as $reply)
-    {{ $reply->content }}<i class="far fa-thumbs-up"></i><br>
+    {{ $reply->content }}<br>
+    {{ $reply->created_at->format('Y年m月d日 H:i:s') }}<i class="far fa-thumbs-up"></i><br>
     @endforeach
   </form>
 </div>
